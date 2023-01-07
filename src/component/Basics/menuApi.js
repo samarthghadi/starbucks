107799c6b301cf2cbf7c819c3f2d1c75575f05cc
Samarth Ghadi <samarthghadi@gmail.com>
2023-01-07 01:32:19 -0500

    first commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,148 @@
+const Menu = [
+    {
+        id: 1,
+        image : "images/pike.jpg",
+        name: "Pike",
+        category: "coffee",
+        price: "$3",
+        description: "From our first store in Seattle’s Pike Place Market to our coffeehouses around the world, customers requested a freshly brewed coffee they could enjoy throughout the day. In 2008 our master blenders and roasters created this for you—a smooth, well-rounded blend of Latin American coffees with subtly rich flavours of chocolate and toasted nuts, it’s served fresh every day at a Starbucks® store near you.",
+    },
+    {
+        id: 2,
+        image : "images/blonde.jpg",
+        name: "Blonde",
+        category: "coffee",
+        price: "$3",
+        description: "Lightly roasted coffee that's soft, mellow and flavourful. Easy-drinking and delicious with milk, sugar or flavours like vanilla, caramel or hazelnut.",
+    },
+    {
+        id: 3,
+        image : "images/americano.jpg",
+        name: "Brown Sugar Oat Americano",
+        category: "hot drink",
+        price: "$6",
+        description: "Starbucks Blonde® espresso, brown-sugar syrup and water topped with a steamed oat beverage and dashes of cinnamon for an energizing treat to power you through your day.",
+    },
+    {
+        id: 4,
+        image : "images/cappuccino.jpg",
+        name: "Cappuccino",
+        category: "hot drink",
+        price: "$6",
+        description: "Dark, rich espresso lies in wait under a smoothed and stretched layer of thick milk foam. An alchemy of barista artistry and craft."
+    },
+    {
+        id: 5,
+        image : "images/caramel.jpg",
+        name: "Caramel Macchiato",
+        category: "hot drink",
+        price: "$6",
+        description: "Freshly steamed milk with vanilla-flavoured syrup marked with espresso and topped with a caramel drizzle for an oh-so-sweet finish."
+    },
+    {
+        id: 6,
+        image : "images/flatwhite.jpg",
+        name: "Flat White",
+        category: "hot drink",
+        price: "$6",
+        description: "Smoothristretto shots of espresso get the perfect amount of steamed whole milk to create a not-too-strong, not-too-creamy, just-right flavour."
+    },
+    {
+        id: 7,
+        image : "images/hotchoc.jpg",
+        name: "Hot Chocolate",
+        category: "hot drink",
+        price: "$6",
+        description: "Steamed milk with vanilla- and chocolate-flavoured syrups. Topped with sweetened whipped cream and chocolate-flavoured drizzle. A timeless classic made to sweeten your spirits."
+    },
+    {
+        id: 8,
+        image : "images/latte.jpg",
+        name: "Caramel Brûlé Latte",
+        category: "hot drink",
+        price: "$6",
+        description: "Our signature espresso, steamed milk and rich caramel brûlé sauce finished with whipped cream and a supreme topping of even more caramel brûlé bits."
+    },
+    {
+        id: 9,
+        image : "images/whitemocha.jpg",
+        name: "White Chocolate Mocha",
+        category: "hot drink",
+        price: "$6",
+        description: "Our signature espresso meets white chocolate sauce and steamed milk, and then is finished off with sweetened whipped cream to create this supreme white chocolate delight."
+    },
+    {
+        id: 10,
+        image : "images/chai.jpg",
+        name: "Chai Tea Latte",
+        category: "tea",
+        price: "$6",
+        description: "Black tea infused with cinnamon, clove and other warming spices, combined with steamed milk and topped with foam for the perfect balance of sweet and spicy. An iconic chai cup."
+    },
+    {
+        id: 11,
+        image : "images/irishcream.jpg",
+        name: "Irish Cream Cold Brew",
+        category: "cold coffee",
+        price: "$5",
+        description: "Everything and more that you ever wanted in a sippable, dreamy holiday drink: Starbucks® Cold Brew swirled with Irish cream syrup, and topped with a cloud of vanilla sweet cream cold foam and a hint of cocoa."
+    },
+    {
+        id: 12,
+        image : "images/mangodragon.jpg",
+        name: "MangoDragon Lemonade",
+        category: "refresher",
+        price: "$7",
+        description: "This tropical-inspired pick-me-up—crafted with a clever combination of vibrant lemonade, sweet mango and refreshing dragonfruit flavours and hand-shaken with ice plus a scoop of real diced dragonfruit."
+    },
+    {
+        id: 13,
+        image : "images/strawacai.jpg",
+        name: "Strawberry Acai Lemonade",
+        category: "refresher",
+        price: "$7",
+        description: "Sweet strawberry flavours, passion fruit and açaí notes balanced with the delightful zing of lemonade, served over ice with freeze-dried strawberry pieces. The ultimate pick-me-up your afternoon is calling for."
+    },
+    {
+        id: 14,
+        image : "images/pineapple.jpg",
+        name: "PineApple Lemonade",
+        category: "refresher",
+        price: "$7",
+        description: "Tropical flavours of pineapple and passionfruit combine with diced pineapple and refreshing lemonade to create a taste of sunshine in a cup."
+    },
+    {
+        id: 15,
+        image : "images/cookiecrumble.jpg",
+        name: "Mocha Cookie Crumble Frappuccino",
+        category: "frappuccino",
+        price: "$8",
+        description: "Frappuccino Roast coffee, mocha sauce and Frappuccino chips blended with milk and ice, layered on top of whipped cream and chocolate cookie crumble and topped with vanilla whipped cream, mocha drizzle and even more chocolate cookie crumble. Each sip is as good as the last . . . all the way to the end."
+    },
+    {
+        id: 16,
+        image : "images/caramelribbon.jpg",
+        name: "Caramel Ribbon Crunch Frappuccino",
+        category: "frappuccino",
+        price: "$8",
+        description: "Buttery caramel syrup blended with coffee, milk and ice, then topped with a layer of dark caramel sauce, whipped cream, caramel drizzle and a crunchy caramel-sugar topping—oh-so-beautifully delicious."
+    },
+    {
+        id: 17,
+        image : "images/strawfrap.jpg",
+        name: "Strawberry Crème Frappuccino",
+        category: "frappuccino",
+        price: "$8",
+        description: "Summer's favourite berry is the star of this delicious Frappuccino Blended Beverage—a blend of ice, milk and strawberry puree layered on top of a splash of strawberry puree and finished with vanilla whipped cream."
+    },
+    {
+        id: 18,
+        image : "images/matchafrap.jpg",
+        name: "Matcha Crème Frappuccino",
+        category: "frappuccino",
+        price: "$8",
+        description: "This blend of sweetened premium matcha green tea, milk and ice—topped off with sweetened whipped cream—inspires a delicious boost and good green vibes."
+    }
+];
+
+export default Menu;
